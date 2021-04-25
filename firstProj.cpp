c9a7c0ed6f095f721906db400d8cd1216d5a1c9d
Monika <monikaangel.ma15@gmail.com>
2021-04-25 01:52:45 -0700

    Learned how to create if statments and doing increase to decrease for inputs from user

--- firstProj.cpp
+++ firstProj.cpp
@@ -47,17 +47,17 @@ int main()
     // std::cout << "Multiplied by itself : " << squared << std::endl;
     // std::cout << "ohh babi a triple : " << number * number * number << std::endl; // you can do operations inside these too
 
-    int a, b, c, d;
+    // int a, b, c, d;
 
-    std::cout << "Enter 4 numbers: ";
-    std::cin >> a >> b >> c >> d;
+    // std::cout << "Enter 4 numbers: ";
+    // std::cin >> a >> b >> c >> d;
 
-    int sum = a + b + c + d;
+    // int sum = a + b + c + d;
 
-    float average = sum / 5.0;
-    //if we divide by an integer then itll give us an integer, so we have to turn to float
+    // float average = sum / 5.0;
+    // //if we divide by an integer then itll give us an integer, so we have to turn to float
 
-    std::cout << "Average is :" << average << std::endl;
+    // std::cout << "Average is :" << average << std::endl;
 
     //const means u cant change
     //ex: const int y = 3; we cant change
@@ -73,6 +73,88 @@ int main()
     int var3{20};
     //different and new ways to initialize variables
     */
+
+   /*
+   this using namespace std replaces it through the code without having
+   to rewrite it after every output
+    #include <iostream>
+    #include <vector>
+    #include <string>
+
+    using namespace std;
+
+
+    int main()
+    {
+    cout << "hello" << endl;
+    }
+   */
+  //if else statement
+  /*using namespace std;
+
+    int x = 10;
+
+    if(x > 6)
+    {
+        cout << x << " is greater than 6 " <<  endl;
+    }
+    if(10)
+    {
+        cout << x << " is true " <<  endl;
+    }
+    if(-4)
+    {
+        cout <<" -4 is also true " <<  endl;
+    }
+    //we arent comparing to anything so it just prints it out
+    //its as if we are saying if(-4 == -4)
+    if(x > 10)
+    {
+        cout << "this is true" << endl;
+    }
+    else
+        cout << x << " is none of the if statments " << endl;
+    cout << "outside the for loop" << endl;
+    
+    //when we use if{} then if{} instead of else if, it runs through every one of 
+    //the if statments that is true but if we do else if then it just 
+    //runs through the first true statement, returns then stop
+
+    */
+
+   int age;
+
+   using namespace std;
+
+   cout << "Enter persons age: " << endl;
+
+   cin >> age;
+//for if statements we have to go from increase to decrease or else its
+//not going to work and its going to grab the first one its related to 
+//and ignore everything else that it was suppose to go through
+
+    if(age >= 21)
+    {
+        cout << "Person is a full legal adult and can drink" << endl;
+    }
+
+    else if(age >= 18)
+    {
+        cout << "Person is an adult but cant drink alcohol" << endl;
+    }
+
+    else if(age > 13)
+    {
+        cout << "Person is a teenager" << endl;
+    }
+
+    else if (age > 3)
+    {
+        cout << "Person is a kid" << endl;
+    }
+    else
+    cout << "Its a DA BABY" << endl;
+
 
     return 0;
 
